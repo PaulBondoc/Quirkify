8c12f171685a75e465dd8f783405057d124dbb65
Paul Bondoc <pjbndc@gmail.co>
2024-03-06 20:19:35 +0800

    removed unused lines

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./Routes";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { useEffect } from "react";
 
 function App() {
   return (
