d2d5c46cdfb49924e98f5c84d363e9a0a6a7d764
Paul Bondoc <pjbndc@gmail.co>
2024-03-07 08:58:56 +0800

    animation test

--- src/test/testData.tsx
+++ src/test/testData.tsx
@@ -11,4 +11,10 @@ export const sampleImageData: string[] = [
   "https://media.rawg.io/media/games/49c/49c3dfa4ce2f6f140cc4825868e858cb.jpg",
   "https://media.rawg.io/media/games/b8c/b8c243eaa0fbac8115e0cdccac3f91dc.jpg",
   "https://media.rawg.io/media/games/bc0/bc06a29ceac58652b684deefe7d56099.jpg",
+  "https://media.rawg.io/media/games/20a/20aa03a10cda45239fe22d035c0ebe64.jpg",
+  "https://media.rawg.io/media/games/618/618c2031a07bbff6b4f611f10b6bcdbc.jpg",
+  "https://media.rawg.io/media/games/2ba/2bac0e87cf45e5b508f227d281c9252a.jpg",
+  "https://media.rawg.io/media/games/736/73619bd336c894d6941d926bfd563946.jpg",
+  "https://media.rawg.io/media/games/021/021c4e21a1824d2526f925eff6324653.jpg",
+  "https://media.rawg.io/media/games/2ba/2bac0e87cf45e5b508f227d281c9252a.jpg",
 ];
